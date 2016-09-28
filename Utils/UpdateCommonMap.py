6277f12b444cadc9c3795d1424d7cc3de105517e
Chris Hesik <christopher.hesik@amd.com>
2016-09-28 12:26:36 -0400

    Add UpdateCommon script to clone GitHub repos (this will be overridden to clone from git.amd.com in the internal build

---
+++ Utils/UpdateCommonMap.py
@@ -0,0 +1,27 @@
+# UpdateCommonMap.py
+#
+#    Map of GitHub project names to clone target paths, relative to the GPUPerfAPI 
+#    project root on the local disk.
+
+# GitHub GPUOpen-Tools projects map
+GitHubMapping = {
+    "common-dk-GraphViz"                     : "../Common/DK/GraphViz",
+    "common-dk-Doxygen"                      : "../Common/DK/Doxygen",
+    "common-lib-amd-ADL"                     : "../Common/Lib/AMD/ADL",
+    "common-lib-amd-APPSDK-3.0"              : "../Common/Lib/AMD/APPSDK",
+    "common-lib-amd-HSA"                     : "../Common/Lib/AMD/HSA",
+	"common-lib-ext-Boost-1.59"              : "../Common/Lib/Ext/Boost",
+    "common-lib-ext-GoogleTest-1.7"          : "../Common/Lib/Ext/GoogleTest",
+    "common-lib-ext-OpenGL"                  : "../Common/Lib/Ext/OpenGL",
+    "common-lib-ext-OpenGLES"                : "../Common/Lib/Ext/OpenGLES",
+    "common-lib-ext-WindowsKits"             : "../Common/Lib/Ext/Windows-Kits",
+    "common-src-ADLUtil"                     : "../Common/Src/ADLUtil",
+    "common-src-AMDTMutex"                   : "../Common/Src/AMDTMutex",
+    "common-src-AmdDxExt"                    : "../Common/Src/AmdDxExt",
+    "common-src-DeviceInfo"                  : "../Common/Src/DeviceInfo",
+    "common-src-DynamicLibraryModule"        : "../Common/Src/DynamicLibraryModule",
+    "common-src-GPUPerfAPIUtils"             : "../Common/Src/GPUPerfAPIUtils",
+    "common-src-TSingleton"                  : "../Common/Src/TSingleton",
+    "common-src-Vsprops"                     : "../Common/Src/Vsprops",
+}
+
