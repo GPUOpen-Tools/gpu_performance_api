a676f107868d260fa67c7b02896fc631eccbb8bb
Peter Lohrmann <Peter.Lohrmann@amd.com>
2021-07-27 19:16:48 -0700

    Update dependency map script

--- scripts/dependencies_map.py
+++ scripts/dependencies_map.py
@@ -10,19 +10,19 @@
 # "git repo name"  : ["Directory for clone relative to parent project directory",  "branch or commit to checkout (or None for top of tree)"]
 gitMapping = {
  # Lib.
-    "common-lib-amd-ADL"                  : ["../Common/Lib/AMD/ADL",               None],
-    "common-lib-amd-APPSDK-3.0"           : ["../Common/Lib/AMD/APPSDK",            None],
-    "common-lib-ext-OpenGL"               : ["../Common/Lib/Ext/OpenGL",            None],
-    "common-lib-ext-WindowsKits"          : ["../Common/Lib/Ext/Windows-Kits",      None],
-    "googletest"                          : ["../Common/Lib/Ext/GoogleTest",        "v1.8.x"],
+    "common-lib-amd-ADL"                  : ["../Common/Lib/AMD/ADL",               "da98d7a9d2b8a6222d32c82952ee44bdfc827b2e"],
+    "common-lib-amd-APPSDK-3.0"           : ["../Common/Lib/AMD/APPSDK",            "55a6940ebc963daec69152314a1bb94943287d4c"],
+    "common-lib-ext-OpenGL"               : ["../Common/Lib/Ext/OpenGL",            "63cd62c8b25e165b0e6f3307b998ed0cf3810edf"],
+    "common-lib-ext-WindowsKits"          : ["../Common/Lib/Ext/Windows-Kits",      "2c99b88d6a8a9b672464436dad771b9799056a8a"],
+    "googletest"                          : ["../Common/Lib/Ext/GoogleTest",        "542e057c6c5bf45454b43764b881397b71164d62"],
 # Src.
-    "common-src-ADLUtil"                  : ["../Common/Src/ADLUtil",               None],
-    "common-src-AmdDxExt"                 : ["../Common/Src/AmdDxExt",              None],
-    "common-src-AmdVkExt"                 : ["../Common/Src/AmdVkExt",              None],
-    "common-src-DeviceInfo"               : ["../Common/Src/DeviceInfo",            None],
-    "common-src-DynamicLibraryModule"     : ["../Common/Src/DynamicLibraryModule",  None],
-    "common-src-TSingleton"               : ["../Common/Src/TSingleton",            None],
-    "common-src-CMakeModules"             : ["../Common/Src/CMakeModules",          None],
+    "common-src-ADLUtil"                  : ["../Common/Src/ADLUtil",               "d62c94514326775c83fc129bb89d299c8749ebd1"],
+    "common-src-AmdDxExt"                 : ["../Common/Src/AmdDxExt",              "9e79ce1ca673ad977328d0f32a58c2c885efa67f"],
+    "common-src-AmdVkExt"                 : ["../Common/Src/AmdVkExt",              "3488d03b7f3bf59c4f41e3f5056801daec5aaa9d"],
+    "common-src-DeviceInfo"               : ["../Common/Src/DeviceInfo",            "361991effe05e9591bdd8f612f534157f8e5945d"],
+    "common-src-DynamicLibraryModule"     : ["../Common/Src/DynamicLibraryModule",  "e6451ce26b8509cf724c7cf5d007878791143a58"],
+    "common-src-TSingleton"               : ["../Common/Src/TSingleton",            "02e8fa7d98f33cdbd0e1f77d1a8a403a32e35882"],
+    "common-src-CMakeModules"             : ["../Common/Src/CMakeModules",          "4d593266f3bb8916b15834050744659d0b86fc65"],
 }
 
 # Name : Source , Destination, version, Copied/Installation location wrt to Script root
